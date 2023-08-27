11542228f55699e2c270aa6c596db7ac6d9fa071
Andy <andyngkh86@gmail.com>
2023-08-27 21:42:51 +0800

    update

--- src/main/java/com/websocket/app/config/WebSocketConfig.java
+++ src/main/java/com/websocket/app/config/WebSocketConfig.java
@@ -1,11 +1,18 @@
 package com.websocket.app.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
 
 
 @Configuration
@@ -21,8 +28,21 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/api/websocket-end-point")
-                .setAllowedOrigins("http://127.0.0.1","http://www.mysocket.com")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+                List tokenList = accessor.getNativeHeader("Authorization");
+                System.out.println("tokenList: " + tokenList);
+                return message;
+            }
+        });
     }
 
 }
--- website/main.js
+++ website/main.js
@@ -5,13 +5,29 @@ console.log("Version:1.0.3");
 
 
 function connect() {
-    var socket = new SockJS('http://127.0.0.1:8181/api/websocket-end-point');
+    let opts = {
+        'headers' : 
+            { 'Authorization' : 'Andy Token' }
+        
+    };
+    for (var key in opts.headers) {
+        console.log('=====================>>>>');
+        console.log('key,value=(' + key + ',' + opts.headers[key] +')');
+      }
+
+    let header = {
+        'Authorization' : '9743819yeahruqwgrjbsmacbamzsbca9743819yeahruqwgrjbsmacbamzsbca9743819yeahruqwgrjbsmacbamzsbca9743819yeahruqwgrjbsmacbamzsbca9743819yeahruqwgrjbsmacbamzsbca'
+    }
+    var socket = new SockJS('http://127.0.0.1:8181/api/websocket-end-point',{},opts);
+    console.log(socket);
     stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
+    console.log(stompClient);
+    stompClient.connect(opts,function (frame) {
         console.log('Connected: ' + frame);
         let username = "D" + (new Date()).getMilliseconds();
         console.log("username: " + username);
         stompClient.subscribe('/user/' + username + '/msg', function (greeting) {
+            console.log('=========================>');
            console.log("Message from server : " + greeting);
         });
         onUserConnected(username);
